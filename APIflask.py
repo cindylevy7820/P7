8eb4893a197fae4a3baa23ccea1fe75287923eee
cindylevy7820 <132998034+cindylevy7820@users.noreply.github.com>
2023-06-06 16:50:39 +0200

    Update APIflask.py

--- APIflask.py
+++ APIflask.py
@@ -46,10 +46,8 @@ def main() :
 
     @st.cache
     def load_data():
-        z = ZipFile("data/default_risk.zip")
         data = pd.read_csv('data/default_risk.csv', index_col='SK_ID_CURR', encoding ='utf-8')
-
-        z = ZipFile("data/X_sample.zip")
+        
         sample = pd.read_csv('data/X_sample.csv', index_col='SK_ID_CURR', encoding ='utf-8')
         
         description = pd.read_csv("data/features_description.csv", 
